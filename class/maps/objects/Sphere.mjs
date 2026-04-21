d6b0bf5827eab38bb5366ead175b7688e8833b61
BunnyNabbit “Aon” <aon@bunnynabbit.com>
2026-04-20 22:28:45 -0500

    Remove unused import

--- class/maps/objects/Sphere.mjs
+++ class/maps/objects/Sphere.mjs
@@ -1,5 +1,4 @@
 // @ts-check
-/** @import {Vector3} from "../../../types/data.mts" */
 import { Instance } from "./Instance.mjs"
 
 /** I am a collectible in the form of a red sphere. I was probably called a gem before. Can't remember. */
